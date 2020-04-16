8f3aab5a6fd31e4e4f701a462d86ec61c556e50d
pwcodigo <pwcodigo@email.com>
2020-04-16 00:11:41 -0300

    feat:  aula 402 - Projeto API - concluida

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el: "#app",
+    data:{
+        products: [],        
+    },
+    methods: {
+        callApiProducts(){
+           fetch("api/products.json")
+            .then(r => r.json())
+            .then(r => {
+                this.products = r;
+            })
+        }
+    },
+    created(){
+        this.callApiProducts()
+    }
+});
